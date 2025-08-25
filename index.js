c282bba7f0205f472ccf0286a18fd2ea0eb9e760
jehovah-son <chukwukasuccess0@gmail.com>
2025-08-25 18:06:48 +0100

    `Refactor index.js to use const for element references and add comments for clarity.`

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-increasebtn = document.getElementById("increase");
-decreasebtn = document.getElementById("decrease");
-resetbtn = document.getElementById("reset");
-counter = document.getElementById("counter");
+const increasebtn = document.getElementById("increase");
+const decreasebtn = document.getElementById("decrease");
+const resetbtn = document.getElementById("reset");
+const counter = document.getElementById("counter");
 
 let counterValue = 0;
 
+increasebtn.onclick = function () {
+  // Increase the counter value
+  counterValue++;
 
-increasebtn.onclick = function (){
-    counterValue++;
-    counter.textContent = counterValue
+  // Update the counter text content
+  counter.textContent = counterValue;
 
-    console.log(counterValue);
-}
+  // Log the counter value
+  console.log(counterValue);
+};
 
+decreasebtn.onclick = function () {
+  counterValue--;
+  counter.textContent = counterValue;
 
+  console.log(counterValue);
+};
+
+resetbtn.onclick = function () {
+  counterValue = 0;
+  counter.textContent = counterValue;
+
+  console.log(counterValue);
+};
